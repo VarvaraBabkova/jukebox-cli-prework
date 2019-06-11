8af1d937796e10777e14c61e030c10a88b95b2fc
Varvara Babkova <varvara.munday@gmail.com>
2019-06-11 19:26:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,6 +28,7 @@ def play (songs)
       return
     end
   else
+    puts "NUmber"
     if song >= 1 && song <= songs.size #(1..songs.length).include?(song)
       puts "Playing <#{songs[song - 1]}>"
       return
